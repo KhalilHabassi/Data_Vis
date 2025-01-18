73b0f09e609d756837d144b45de2168d3fba0a5c
khaliil habassi <186034376+khalillab-cloud@users.noreply.github.com>
2025-01-18 16:21:31 +0100

    Mise à jour du fichier main.py et ajout de la configuration pre-commit

--- src/package_creation_tutorial/main.py
+++ src/package_creation_tutorial/main.py
@@ -1,4 +1,4 @@
-from package_creation_tutorial.string_ops import reverse_string, reverse_string, count_vowels, capitalize_words
+from package_creation_tutorial.string_ops import reverse_string, count_vowels, capitalize_words
 def main():
     """
     Main function to demonstrate the string operations.
